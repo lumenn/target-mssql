56c24c056f5fd70a1a24f011c6719519801438b5
Maksymilian Lumenn Stachowiak <silmelumenn@gmail.com>
2024-10-01 10:11:57 +0200

    Moved to default implementation of conform_name

--- target_mssql/sinks.py
+++ target_mssql/sinks.py
@@ -8,7 +8,7 @@ from typing import TYPE_CHECKING, Any, Dict, Iterable, List, Optional
 
 import sqlalchemy
 from singer_sdk.connectors.sql import SQLConnector
-from singer_sdk.helpers._conformers import replace_leading_digit, snakecase
+from singer_sdk.helpers._conformers import replace_leading_digit 
 from singer_sdk.sinks.sql import SQLSink
 from sqlalchemy import Column
 
@@ -284,21 +284,3 @@ class mssqlSink(SQLSink):
             db_name, schema_name, table_name = parts
 
         return db_name, schema_name, table_name
-
-    def conform_name(self, name: str, object_type: Optional[str] = None) -> str:
-        """Conform a stream property name to one suitable for the target system.
-        Transforms names to snake case by default, applicable to most common DBMSs'.
-        Developers may override this method to apply custom transformations
-        to database/schema/table/column names.
-        Args:
-            name: Property name.
-            object_type: One of ``database``, ``schema``, ``table`` or ``column``.
-        Returns:
-            The name transformed to snake case.
-        """
-        # strip non-alphanumeric characters, keeping - . _ and spaces
-        name = re.sub(r"[^a-zA-Z0-9_\-\.\s]", "", name)
-        # convert to snakecase
-        name = snakecase(name)
-        # replace leading digit
-        return replace_leading_digit(name)
